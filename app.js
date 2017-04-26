e575b09847232d46d872fe5d413e3c702032e756
Evelyn Máire <evie.ferguson@gmail.com>
2017-04-27 01:30:06 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-// var port = server.listen(process.env.PORT || 3000)
-var port = process.env.PORT || 3000
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html')
-})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-io.on('connection', function (socket) {
-	console.log('a user connected')
-	socket.on('disconnect', function () {
-		console.log('User disconnected')
-		})
-	socket.on('chat message', function (msg) {
-		console.log('message: ' + msg)
-		io.emit('chat message', msg)
-	})
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-http.listen(port, function () {
-	console.log('Listening on port 3000')
-})
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
